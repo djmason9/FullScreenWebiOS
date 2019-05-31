f7420201cc47841bb784b5d886861fb011b979c4
Darren Mason <dmason@axway.com>
2019-05-31 15:16:57 -0700

    added refresh pinch

--- FullScreenWeb/ViewController.swift
+++ FullScreenWeb/ViewController.swift
@@ -1,20 +1,44 @@
 //
 //  ViewController.swift
-//  FullScreenWeb
+//  fullscreenWebBrowser
 //
 //  Created by dmason on 5/31/19.
 //  Copyright © 2019 dmason. All rights reserved.
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        
+        webView = WKWebView()
+//        webView?.isUserInteractionEnabled = false;
+        view = webView
+        
+         self.webView?.scrollView.delegate = self
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let request = URLRequest(url: URL(string: "http://10.129.178.116/alexa-lab-kiosks/admin.htm")!)
+        webView?.load(request)
+//
     }
-
-
+    
+    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+        //secret reload
+        webView?.evaluateJavaScript("window.location.reload(true)", completionHandler: nil);
+        
+        scrollView.pinchGestureRecognizer?.isEnabled = false
+    }
 }
 
