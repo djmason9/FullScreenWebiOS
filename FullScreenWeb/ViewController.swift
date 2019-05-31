123768540024d6249ed6b93f81849e966bd7be1e
Darren Mason <dmason@axway.com>
2019-05-31 13:13:02 -0700

    Initial Commit

---
+++ FullScreenWeb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FullScreenWeb
+//
+//  Created by dmason on 5/31/19.
+//  Copyright © 2019 dmason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
